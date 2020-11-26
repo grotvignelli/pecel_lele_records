c879c2f84ca1cb44e5ffde7d6000bf87ed3f6903
embong <chernomega11@gmail.com>
2020-11-26 23:48:40 +0700

    Setting dockerfile and docker-compose. Added app project and core app.

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+
+from core.models import Artist
+
+
+class RecordsModel(TestCase):
+
+    def test_create_artist(self):
+        """Test create a new artist on the db"""
+        pass
+
+    def test_create_album(self):
+        """Test create a new album on the db"""
+        pass
+
+    def test_create_single(self):
+        """Test create a new single on the db"""
+        pass
